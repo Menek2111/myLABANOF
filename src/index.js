163a7de1d33539ef3205cf4f493c957ae3b7a20f
Menek2111 <bergami50@gmail.com>
2023-05-07 18:22:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import App from './App'
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from "./Login";
